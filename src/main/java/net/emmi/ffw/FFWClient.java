24ea28f1d43acf93116e2b33c084fdf9cba425ab
emmi <transrights1@gmail.com>
2024-11-11 10:29:16 -0700

    Initial commit

---
+++ src/main/java/net/emmi/ffw/FFWClient.java
@@ -0,0 +1,10 @@
+package net.emmi.ffw;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FFWClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/emmi/ffw/FrozenFarmingWasteland.java
@@ -0,0 +1,16 @@
+package net.emmi.ffw;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class FrozenFarmingWasteland implements ModInitializer {
+	public static final String MOD_ID = "frozen-farming-wasteland";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
